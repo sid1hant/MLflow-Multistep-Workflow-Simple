1acefc600ba84589ac017f61fb624f78b3641feb
Siddhant <siddhant99dei@gmail.com>
2022-10-13 12:30:42 +0530

    first commit

---
+++ init_pro.sh
@@ -0,0 +1,4 @@
+conda create --prefix ./env python=3.7 -y
+source E:/Users/SIDDHANT SINGH/Documents/ana/Scripts/conda.exe # use your username instead of sunny
+source activate ./env
+pip install -r requirements.txt
---
+++ stage_02.py
@@ -0,0 +1,7 @@
+import mlflow
+with open("artifacts01.txt", "r") as f:
+    text = f.read()
+
+new_text="end 0f stage 02"
+mlflow.log_param("new_text",new_text)
+print("end of stage 02")
